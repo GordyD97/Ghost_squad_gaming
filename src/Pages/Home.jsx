c442adbb30954173f8bc7d0db5747c14c81abd08
Gordon D <gordon.draine97@yahoo.com>
2023-12-07 22:28:33 -0500

    adding pages and starting home page.

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Header from '../components/Header';
+import Footer from './componentes/Footer';
+
+const Home = () => {
+  return (
+    <>
+    <Header />
+{/* main page where most components will be placed.  */}
+
+
+
+    <Footer />
+
+    </>
+  )
+}
+
+export default Home
