d4c76fdda994adc2df26c938f14850e24ec4bf83
Gordon D <gordon.draine97@yahoo.com>
2023-12-09 23:12:09 -0500

    adding team card components to page

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
 import Header from '../components/Header';
 import Footer from './componentes/Footer';
+import Livematch from './components/Livematch';
+import calendar from '../components/calendar';
+import Schedule from '../components/Schedule'
+// dont know if i need to import the calendar as well when the schedule component.page will have it already.
+
 
 const Home = () => {
   return (
     <>
     <Header />
 {/* main page where most components will be placed.  */}
+    <Livematch />
 
-
+    <Schedule />
+    
 
     <Footer />
 
---
+++ src/Pages/Players.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import PlayerCard from '../components/PlayerCard';
+import Header from '../components/Header';
+import Footer from '../components/Footer'
+
+const Players = () => {
+  return (
+    <>
+    <Header />
+
+
+    <PlayerCard/>
+
+
+
+    <Footer /> 
+    
+    
+    </>
+  )
+}
+
+export default Players
