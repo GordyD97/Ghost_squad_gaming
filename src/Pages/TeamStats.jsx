4c5301ba3db895b7a860b2ab3e1203e95133866c
Gordon D <gordon.draine97@yahoo.com>
2023-12-13 09:37:46 -0500

    adding the data mapping feature for the team stats page

--- src/Pages/TeamStats.jsx
+++ src/Pages/TeamStats.jsx
@@ -1,22 +1,29 @@
-import React from 'react'
+import React from 'react';
+import Header from '../components/Header';
 
-const 
 
-<>
+const TeamStats = () => {
 
-</>= () => {
+const data = [
+
+  { }
+// fake game data will be inserted here the keys and values will consist of KD, wins, number of players. 
+
+
+]
+
   return (
-    <div>
+    <>
+      <Header />
 
-        <>
-        
-        </>
-    </div>
-  )
+      <ul>
+        {data.map((item) => (
+          <li key={item.id}>{InsertEmoticon.name} </li>
+        ))}
+      </ul>
+      {/* above the data will be mapped through all is needed is the array to be set and either pictures and stats to be added.  */}
+    </>
+  );
 }
 
-export default 
-
-<>
-
-</>
+export default TeamStats
