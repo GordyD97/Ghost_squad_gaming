352102f807b2b534b54a4fedf56cb222b2639b36
Gordon D <gordon.draine97@yahoo.com>
2023-12-15 22:21:53 -0500

    adding logo data

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -26,16 +26,16 @@ export default function StickyFooter() {
 
   const logos = [ 
 
-{ id: lOGITECH},
-{ id: DUCKY KEYBOARDS}, 
-{ id: TWTICH},
-{ id: HYPERX}, 
-{ id: COORSAIR}, 
-{ id: ASTRO}, 
-{ id: CALIBAR VAPE},
-{ id: THERMALTAKE}, 
-{ id: NUPHY}, 
-{ id: DISCORD}
+{ id: lOGITECH, src: logitech},
+{ id: DUCKY KEYBOARDS, src: ducky}, 
+{ id: TWTICH, src: twitch},
+{ id: HYPERX, src: hyper}, 
+{ id: COORSAIR, src: coorsair}, 
+{ id: ASTRO, src: astro}, 
+{ id: CALIBAR VAPE, src: vape},
+{ id: THERMALTAKE, src: thermaltake}, 
+{ id: NUPHY, src: nuphy}, 
+{ id: DISCORD, src: discord}
 
 
   ]
