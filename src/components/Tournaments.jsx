22c8ae67db4df213850de7f277315b18891aec90
Gordon D <gordon.draine97@yahoo.com>
2024-01-03 23:17:30 -0500

    adding new tournament card

--- src/components/Tournaments.jsx
+++ src/components/Tournaments.jsx
@@ -1,17 +1,16 @@
-import React from 'react'
-import Paper from '@mui/material/Paper';
+import React from "react";
+import Paper from "@mui/material/Paper";
 import Chip from "@mui/material/Chip";
 
-import Fortnite from '../assets/imgs/fortnite.jpg'
 
+import Fortnite from "../assets/imgs/fortnite.jpg";
+import BoxFightimg from "../assets/imgs/boxfight.jpeg";
 
 const Tournaments = () => {
   return (
     <>
       <Paper elevation={3}>
-        <img src={Fortnite}
-        alt="fotnitebanner"
-        />
+        <img src={Fortnite} alt="fotnitebanner" />
         {/* the component will have dynamic data based on either the game or the date of the up cmoing tournament.  */}
 
         <p> Solo kill race and Duo Surivial tournamnet up coming!</p>
@@ -20,8 +19,20 @@ const Tournaments = () => {
         <Chip label="Kill Race" />
         <Chip label="Battlle Royale" />
       </Paper>
+      <Paper elevation={3}>
+        
+        <img  src={BoxFightimg} alt="fotnitebanner" />
+        {/* the component will have dynamic data based on either the game or the date of the up cmoing tournament.  */}
+
+        <p> Box fight free forall with Match Bounty</p>
+        <Chip label="Solos" />
+        <Chip label="Cash prize" />
+        <Chip label="Box fights" />
+        <Chip label="Battlle Royale" />
+        <Chip label="Bounty" />
+      </Paper>
     </>
   );
-}
+};
 
-export default Tournaments
+export default Tournaments;
