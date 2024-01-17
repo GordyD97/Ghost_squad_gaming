a1b5f764418afc4ec5797477928d9ed3007cbb65
Gordon D <gordon.draine97@yahoo.com>
2024-01-16 20:39:36 -0500

    adding events to schedule page adding test code in commented section.

--- src/components/Schedule.jsx
+++ src/components/Schedule.jsx
@@ -50,12 +50,50 @@ const EsportsSchedule = () => {
     //   </TableContainer>
     // </Container>
 
-
-<FullCalendar
-        plugins={[ dayGridPlugin ]}
-        initialView="dayGridMonth"
-      />
+    <FullCalendar
+      plugins={[dayGridPlugin]}
+      initialView="dayGridMonth"
+      events={[
+        { title: "Friday Night Box Fights", date: "2024-01-26" },
+        { title: "Satruday Frag", date: "2024-01-27" },
+      ]}
+      // above the events are added into the code. With the events being hard coded there needs to be a link for the "client"- or site admin 
+      // other than me the developer. there either needs to be a link to an admin ui dashboard or 
+    />
   );
 };
 
 export default EsportsSchedule;
+
+
+
+/*     
+
+newEventFill () => {
+  this function will be one of the main connections to the admin page. This function allows the admin to update the date, title and link page to the date selected.
+CRUD operator will be used here 
+};
+
+
+<FullCalendar
+      plugins={[dayGridPlugin]}
+      initialView="dayGridMonth"
+      events={[
+        { title: "Friday Night Box Fights", date: "2024-01-26" },
+        { title: "Satruday Frag", date: "2024-01-27" },
+      ]}
+      
+    />
+      events={(newEventFill)[
+        { title:`${event.title}`, date: `${event.date}` a href=`${event.link}`},
+        { title: `${event.title}`, date: `${event.date}` a href=`${event.link}`},
+      ]}}
+      
+    />
+
+
+
+
+
+
+*/
