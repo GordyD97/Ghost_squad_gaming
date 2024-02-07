e33024fe57124c1d274deb1733b0c7eb5aa7ced8
Gordon D <gordon.draine97@yahoo.com>
2024-02-06 22:44:37 -0500

    adding more to player page

--- src/Pages/Players.jsx
+++ src/Pages/Players.jsx
@@ -4,20 +4,34 @@ import Header from '../components/Header';
 import Footer from '../components/Footer'
 
 const Players = () => {
+
+// players data object will be created here . 
+
+
+
   return (
     <>
-    <Header />
+      <Header />
 
+      <PlayerCard />
+     
 
-    <PlayerCard/>
+      {players.map(({ id, src }) => (
+        <Grid item xs={2} sm={4} md={3} key={id}>
+          <img
+            src={src}
+            alt={id}
+            width="75px"
 
+            // figure out how to do the box shadow style it is listed as "paper on mui. "
+          />
+          <p key={id}>{id}</p>
+        </Grid>
+      ))}
 
-
-    <Footer /> 
-    
-    
+      <Footer />
     </>
-  )
+  );
 }
 
 export default Players
